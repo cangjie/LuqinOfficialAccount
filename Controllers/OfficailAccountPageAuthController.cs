f475fc00d4bee845fd112cbec0cca0c3ebdf8083
cangjie <cangjie@hotmail.com>
2022-04-01 14:13:07 +0800

    page auth start

---
+++ Controllers/OfficailAccountPageAuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using LuqinOfficialAccount.Models;
+using System.Web;
+namespace LuqinOfficialAccount.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class OfficailAccountPageAuthController : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        private readonly IConfiguration _config;
+
+        private readonly Settings _settings;
+        public OfficailAccountPageAuthController(AppDBContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+            _settings = Settings.GetSettings(_config);
+        }
+
+        [HttpGet]
+        public void RedirctToGetCode(string callBackUrl, string state="")
+        {
+            callBackUrl = Util.UrlEncode(callBackUrl);
+            string redirectUrl = Request.Scheme.Trim() + "://" + Request.Host.ToString()
+                + "/OfficailAccountPageAuthController/GetCode?callBack=" + callBackUrl.Trim();
+            string url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + _settings.appId.Trim()
+                + "redirect_uri=" + Util.UrlEncode(redirectUrl) 
+                + "&response_type=code&scope=snsapi_base&state=" + state + "#wechat_redirect";
+            Response.Redirect(url);
+        }
+    }
+}
--- Util.cs
+++ Util.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 namespace LuqinOfficialAccount
 {
     public class Util
@@ -7,6 +8,10 @@ namespace LuqinOfficialAccount
         {
             TimeSpan ts = currentDateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        }
+        public static string UrlEncode(string urlStr)
+        {
+            return HttpUtility.UrlEncode(urlStr.Trim().Replace(" ", "+").Replace("'", "\""));
         }
     }
 }
