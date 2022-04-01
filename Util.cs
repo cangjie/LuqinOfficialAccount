5e5aee6adf5e8aa8889676996d59dcad33d9267e
cangjie <cangjie@hotmail.com>
2022-04-01 11:18:10 +0800

    QRCode

---
+++ Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LuqinOfficialAccount
+{
+    public class Util
+    {
+        public static string GetLongTimeStamp(DateTime currentDateTime)
+        {
+            TimeSpan ts = currentDateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        }
+    }
+}
