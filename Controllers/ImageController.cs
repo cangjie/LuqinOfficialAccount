1724339448a00bcfbf47ff2066b664353f840122
cangjie <cangjie@hotmail.com>
2022-04-06 18:40:55 +0800

    create poster automaticlly

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -6,13 +6,34 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Pipelines;
 using System.Collections;
+using ThoughtWorks.QRCode.Codec;
 namespace LuqinOfficialAccount.Controllers
 {
     [Route("api/[controller]/[Action]")]
     public class ImageController:ControllerBase
     {
         [HttpGet]
-        public void DrawQrCode()
+        public void CreatePersonalPosterWithTextQrCode(string templatePath, int x, int y, int scale, string qrCodeText)
+        {
+            string realTemplatePath = Util.workingPath + templatePath;
+            //Bitmap bmpTemplate = Bitmap.FromFile(realTemplatePath);
+            Image imgTemplate = Bitmap.FromFile(realTemplatePath);
+            QRCodeEncoder enc = new QRCodeEncoder();
+            Bitmap bmpQr = enc.Encode(qrCodeText);
+            Graphics g = Graphics.FromImage(imgTemplate);
+            g.DrawImage(bmpQr, x, y, scale, scale);
+            g.Save();
+            Response.ContentType = "image/jpeg";
+            PipeWriter pw = Response.BodyWriter;
+            Stream s = pw.AsStream();
+            imgTemplate.Save(s, ImageFormat.Jpeg);
+            s.Close();
+            g.Dispose();
+            bmpQr.Dispose();
+            imgTemplate.Dispose();
+        }
+        [HttpGet]
+        public void DrawQrCodeTest()
         {
             string imgPath = Util.workingPath + "/images/a.jpg";
             ArrayList fileArr = new ArrayList();
