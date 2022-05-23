dcae917568ae21d035974dd594a924128fccd5a8
cangjie <cangjie@hotmail.com>
2022-05-23 20:01:36 +0800

    del cdata

--- Controllers/OfficailAccountReply.cs
+++ Controllers/OfficailAccountReply.cs
@@ -119,7 +119,7 @@ namespace LuqinOfficialAccount.Controllers
                 + "<FromUserName ><![CDATA[" + _settings.originalId.Trim() + "]]></FromUserName>"
                 + "<CreateTime >" + Util.GetLongTimeStamp(DateTime.Now) + "</CreateTime>"
                 + "<MsgType><![CDATA[text]]></MsgType>"
-                + "<Content><![CDATA[" + message.Trim() + "]]></Content>"
+                + "<Content>" + message.Trim() + "</Content>"
                 + "</xml>");
             }
             else
