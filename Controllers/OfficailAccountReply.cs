80a10cf814b44a8c2ed4166d15df86db58d8962a
cangjie <cangjie@hotmail.com>
2022-06-21 17:22:54 +0800

    new thumb

--- Controllers/OfficailAccountReply.cs
+++ Controllers/OfficailAccountReply.cs
@@ -171,7 +171,7 @@ namespace LuqinOfficialAccount.Controllers
                 FromUserName = _settings.originalId,
                 ToUserName = _message.FromUserName.Trim(),
                 Content = "b4jyA__yqy1crwzwSktKUwMf04GKGqzAjPbM2PmGdmFY13sBy7otre5t23h55w33",
-                Thumb = "DKLqmuqTxW5A3Bbrn9Ff79gAPtxGjxyhB19G_LHxPuxX6CH7-W9EnI4UEE0i4Zr1"
+                Thumb = "DKLqmuqTxW5A3Bbrn9Ff78ARoT-N-m9sVRyTVD4n66vfEUeeb9jkW6WC2QWPxnUE"
             };
             api.SendServiceMessage(sendMessage);
             /*
