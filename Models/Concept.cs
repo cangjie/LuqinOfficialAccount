7cab860e61ef490c9b6e8e72fbde968513e67267
cangjie <cangjie@hotmail.com>
2023-05-11 16:33:54 +0800

    concept

---
+++ Models/Concept.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LuqinOfficialAccount.Models
+{
+    [Table("concept")]
+    public class Concept
+    {
+        [Key]
+        public int Id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public DateTime list_date { get; set; }
+        public string type { get; set; }
+        public DateTime update_date { get; set; } = DateTime.Now;
+
+    }
+}
