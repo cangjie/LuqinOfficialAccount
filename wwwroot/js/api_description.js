5c06866a88e5d9be4eb36bc5086b5335547e6bbe
cangjie <cangjie@hotmail.com>
2023-06-28 16:44:35 +0800

    open high

--- wwwroot/js/api_description.js
+++ wwwroot/js/api_description.js
@@ -159,4 +159,11 @@
         'defaultCountDays': 15,
         'defaultSort': '缩量'
     },
+    {
+        'name': '涨停后调整收到涨停之上不超过三天再次涨停',
+        'description': '',
+        'url': '/api/LimitUp/GetLimitUpAdjustSettleOverHighestAndLimitUpAgainOpenHigh',
+        'defaultCountDays': 15,
+        'defaultSort': '缩量'
+    }
 ];
