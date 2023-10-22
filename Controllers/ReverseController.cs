169e35cd1a4930ebe5e90c2f882552931dbe6d3c
cangjie <cangjie@hotmail.com>
2023-10-22 11:31:56 +0800

    open high

--- Controllers/ReverseController.cs
+++ Controllers/ReverseController.cs
@@ -301,6 +301,72 @@ namespace LuqinOfficialAccount.Controllers
 
             }
         }
+
+        [HttpGet("{days}")]
+        public async Task<ActionResult<StockFilter>> OpenHigh(int days, DateTime startDate, DateTime endDate, string sort = "代码")
+        {
+            startDate = Util.GetLastTransactDate(startDate, 1, _db);
+            endDate = Util.GetLastTransactDate(endDate, 1, _db);
+            DataTable dt = new DataTable();
+            dt.Columns.Add("日期", Type.GetType("System.DateTime"));
+            dt.Columns.Add("代码", Type.GetType("System.String"));
+            dt.Columns.Add("名称", Type.GetType("System.String"));
+            dt.Columns.Add("信号", Type.GetType("System.String"));
+            dt.Columns.Add("买入", Type.GetType("System.Double"));
+            StockFilter reverseList = (StockFilter)((OkObjectResult)(await limitUpHelper.Reverse(1, startDate, endDate, sort)).Result).Value;
+            for (int i = 0; reverseList != null && i < reverseList.itemList.Count; i++)
+            {
+                Stock s = Stock.GetStock(reverseList.itemList[i].gid);
+                try
+                {
+                    s.ForceRefreshKLineDay();
+                }
+                catch
+                {
+                    continue;
+                }
+                int alertIndex = s.GetItemIndex(reverseList.itemList[i].alertDate.Date);
+                if (alertIndex < 2 || alertIndex >= s.klineDay.Length - 1)
+                {
+                    continue;
+                }
+                double buyPrice = 0;
+                double open = s.klineDay[alertIndex + 1].open;
+                double settle = s.klineDay[alertIndex].settle;
+
+                if (open < settle)
+                {
+                    continue;
+                }
+                if ((open - settle) / settle < 0.01)
+                {
+                    buyPrice = open;
+                }
+                else
+                {
+                    buyPrice = settle * 1.01;
+                }
+
+                DataRow dr = dt.NewRow();
+                dr["日期"] = s.klineDay[alertIndex + 1].settleTime.Date;
+                dr["代码"] = s.gid.Trim();
+                dr["名称"] = s.name.Trim();
+                dr["信号"] = "";
+                dr["买入"] = buyPrice;
+                dt.Rows.Add(dr);
+
+            }
+            StockFilter sf = StockFilter.GetResult(dt.Select("", "日期 desc, " + sort), days);
+            try
+            {
+                return Ok(sf);
+            }
+            catch
+            {
+                return NotFound();
+
+            }
+        }
     }
 }
 
