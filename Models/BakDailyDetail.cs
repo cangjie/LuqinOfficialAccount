b8e70512995b1e28dc1b29c8bb808993db7a7989
cangjie <cangjie@hotmail.com>
2023-12-18 17:13:22 +0800

    no use

---
+++ Models/BakDailyDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LuqinOfficialAccount.Models
+{
+	[Table("bak_daily_detail")]
+	public class BakDailyDetail
+	{
+		[Key]
+		public int id { get; set; }
+		public string gid { get; set; }
+		public string name { get; set; }
+		public DateTime alert_date { get; set; }
+		public double price { get; set; }
+		public double vol { get; set; }
+		public double buying { get; set; }
+		public double selling { get; set; }
+		public double strength { get; set; }
+		public double activity { get; set; }
+		public double attack { get; set; }
+		public double avg_price { get; set; }
+		public double avg_turn_over { get; set; }
+		public DateTime create_date { get; set; }
+	}
+}
+
